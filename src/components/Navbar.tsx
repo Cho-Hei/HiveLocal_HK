cc20cb2edfb6f89f01c9b537939d743b61069b6b
Cho Hei Pei <z5296007@ad.unsw.edu.au>
2025-03-28 12:32:51 +0800

    rename application

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <nav className='navbar relative px-2 w-full bg-[#2E236C] text-primary'>
             <div className='flex flex-grow text-center items-center justify-between'>
-                <h1>HK Map</h1>
+                <h1>HiveLocal HK</h1>
                 <div className='flexCenter'>
                     <WeatherWarning />
                     <Settings />
